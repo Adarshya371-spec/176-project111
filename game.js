505b3fc788c38f2cdd26f84c18437c5c8f530d4f
Adarshya <77096378+Adarshya371-spec@users.noreply.github.com>
2023-06-01 15:16:30 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,35 @@
+
+let words = [
+    {
+        "inputs": 5,
+        "category": "Sports",
+        "word": "Chess"
+    },
+    {
+        "inputs": 6,
+        "category": "European Country Name",
+        "word": "France"
+    },
+
+]
+
+$(document).ready(function () {
+    fillBlanks();
+})
+
+function fillBlanks() {
+    //Select a random word
+  
+        }
+       
+        if (life > 0 && correctGuess!=true && gameOver!=true) {           
+            life = life - 1
+            $("#life").text(life)
+        }
+        else if (life == 0) {
+            $("#result").text("You Lost!!")
+        }
+    })
+}
+
+    
